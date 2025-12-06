d3c178900c54d142ca546e0a22c75bc5ba773dd3
Rishika Kumari <rishikakumari000brj@gmail.com>
2025-12-06 23:27:46 +0530

    Add files via upload

---
+++ VariableInitialization.py
@@ -0,0 +1,13 @@
+a=10
+b=3.5
+c="hello"
+d=True
+e=[1,2,3]
+print("the type of a:",type(a))
+print("the type of b:",type(b))
+print("the type of c:",type(c))
+print("the type of d:",type(d))
+print("the type of e:",type(e))
+
+n=(input("enter  the vaiable"))
+print("type",type(n))
